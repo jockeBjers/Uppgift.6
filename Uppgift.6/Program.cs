44c1246840af8773a3ed3721381135bbb182d5ee
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2024-09-30 16:30:57 +0200

    Call methods from another method to keep Main clean

--- Uppgift.6/Program.cs
+++ Uppgift.6/Program.cs
@@ -4,30 +4,32 @@ namespace Uppgift._6
 {
     internal class Program
     {
-        private static List<string> names = new List<string> { "Anna", "John", "Alice", "Bob" };
+        private static List<string> names = new List<string> { "Anna", "John", "Alice", "Bob" }; // Put list outside of main for easier access.
         static void Main(string[] args)
+        {
+            RunProgram();
+        }
+
+        public static void RunProgram()  // Starts the program to keep the main clear.
         {
             PrintOriginalList();
             PrintSortedList();
             SearchName();
-         
-
-          
         }
 
         public static void PrintOriginalList()
         {
             Console.WriteLine("Original list:");
-            foreach (var name in names)
+            foreach (var name in names) // loops through the original list and prints out the names
             {
                 Console.WriteLine(name);
             }
         }
         public static void PrintSortedList()
         {
-            names.Sort();  // Sort the names alphabetically
+            names.Sort();  // Sort the names in alphabetical order
             Console.WriteLine("\nSorted list:");
-            foreach (var name in names)
+            foreach (var name in names) // loops through the sorted names and prints them out.
             {
                 Console.WriteLine(name);
             }
